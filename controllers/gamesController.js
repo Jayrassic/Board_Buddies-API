87555804e4b8e3c3b88b0f4827236f83b7a59055
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-01-30 16:48:25 -0500

    Added placeholder controllers and routes

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,9 +1,28 @@
 const Game = require("../models/game");
 
+// eturns all games
 exports.allGames = async (req, res, next) => {
   try {
     const data = await Game.find().populate("user", "userName").exec();
     res.json(data);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+// Adds game to all game list
+exports.addGame = async (req, res, next) => {
+  try {
+    res.send("Game Added");
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+// Deletes game from all game list
+exports.deleteGame = async (req, res, next) => {
+  try {
+    res.send("Delete Game");
   } catch (err) {
     res.send(err);
   }
--- routes/games.js
+++ routes/games.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// Get all Games
 router.get("/", gameController.allGames);
+
+// Add game to list
+router.post("/", gameController.addGame);
+
+// Delete game from List
+router.delete("/", gameController.deleteGame);
