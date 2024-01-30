89222df45e8c59fcafcb285f25efcf6186c0c34f
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-01-30 11:14:27 -0500

    Inital commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_KEY);
+}
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: { type: String, required: true },
+  hash: { type: String, required: true },
+  admin: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("User", userSchema);
