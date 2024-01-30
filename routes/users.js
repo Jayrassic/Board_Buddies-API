d7fcfbac7a93869e29c73bcd9d998361d4af2123
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-01-30 17:11:00 -0500

    Created placeholder routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userController = require("../controllers/userController");
+
+// Sign Up User
+router.post("/signup", userController.signupUser);
+
+// login User
+router.post("/login", userController.loginUser);
 
 module.exports = router;
