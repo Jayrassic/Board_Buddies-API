d8ae1845b94df3325c3dd84df9f87b825bdd5adb
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-01 13:00:18 -0500

    Added get userGames

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -2,23 +2,36 @@ const Game = require("../models/games");
 
 // Returns all games
 exports.allGames = async (req, res, next) => {
-  //   try {
-  //     const data = await Game.find().populate("user", "userName").exec();
-  //     res.json(data);
-  //   } catch (err) {
-  //     res.send(err);
-  //   }
-
   try {
-    res.send("All Games");
+    const data = await Game.find().populate("owner", "userName").exec();
+    res.json(data);
   } catch (err) {
     res.send(err);
+  }
+};
+
+// Returns users games list
+exports.userGames = async (req, res, next) => {
+  const userID = req.params.user;
+  // req.user added to request by requireAuth. Contains _id and userName.
+  const tokenInfo = req.user;
+
+  if (userID === tokenInfo.userName) {
+    try {
+      const usersGames = await Game.find({ owner: tokenInfo._id });
+      res.status(200).json(usersGames);
+    } catch (err) {
+      res.status(404).send("No Games Found");
+    }
+  } else {
+    res.status(401).send("You are not authorized to view this info");
   }
 };
 
 // Adds game to all game list
 exports.addGame = async (req, res, next) => {
   try {
+    console.log(req.user);
     res.send("Game Added");
   } catch (err) {
     res.send(err);
