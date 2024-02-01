360b36bea993cdf6fa447d5b9b79da046cfbc877
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-01 13:16:01 -0500

    Added user game routes

--- routes/games.js
+++ routes/games.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const gameController = require("../controllers/gamesController");
+const requireAuth = require("../middleware/requireAuth");
 
 // Get all Games
 router.get("/", gameController.allGames);
+
+// Get users games
+router.get("/:user", requireAuth, gameController.userGames);
 
 // Add game to list
 router.post("/", gameController.addGame);
