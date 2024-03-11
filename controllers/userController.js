d591ed886b9041067a7a6316b710df307f625c4c
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-11 15:43:16 -0400

    Added express-validator. Sanitized and validated inputs. Changed error reporting

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/users");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 
 const createToken = (_id, userName) => {
   return jwt.sign({ _id, userName }, process.env.TOKEN_KEY, {
@@ -8,33 +9,83 @@ const createToken = (_id, userName) => {
 };
 
 // Sign Up User
-exports.signupUser = async (req, res, next) => {
-  const { email, password, userName } = req.body;
+exports.signupUser = [
+  body("userName")
+    .trim()
+    .isLength({ min: 2, max: 10 })
+    .escape()
+    .withMessage("User Name must be between 2 and 10 characters long."),
+  body("password")
+    .trim()
+    .escape()
+    .isStrongPassword({
+      minLength: 8,
+      minNumbers: 1,
+      minUppercase: 1,
+      minSymbols: 1,
+    })
+    .withMessage(
+      "Password must include one uppercase, one number, and one symbol."
+    ),
+  body("email")
+    .trim()
+    .escape()
+    .isEmail()
+    .withMessage("Please enter a valid email address."),
 
-  try {
-    const user = await User.signUp(email, password, userName);
+  async (req, res, next) => {
+    const errors = validationResult(req);
 
-    const token = createToken(user._id, user.userName);
+    const { email, password, userName } = req.body;
 
-    res.status(200).json({ email, token, userName });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
+    if (!errors.isEmpty()) {
+      const cleanErrors = errors.errors.map((error) => error.msg);
+      console.log(cleanErrors);
+      res.status(400).json({ error: cleanErrors });
+    } else {
+      try {
+        const user = await User.signUp(email, password, userName);
+
+        const token = createToken(user._id, user.userName);
+
+        res.status(200).json({ email, token, userName });
+      } catch (err) {
+        res.status(400).json({ error: [err.message] });
+      }
+    }
+  },
+];
 
 // Login user
-exports.loginUser = async (req, res, next) => {
-  const { email, password } = req.body;
+exports.loginUser = [
+  body("password").trim().escape(),
+  body("email")
+    .trim()
+    .escape()
+    .isEmail()
+    .withMessage("Please enter a valid email address."),
 
-  try {
-    const user = await User.login(email, password);
-    const userName = user.userName;
+  async (req, res, next) => {
+    const { email, password } = req.body;
 
-    // create token
-    const token = createToken(user._id, user.userName);
+    const errors = validationResult(req);
 
-    res.status(200).json({ email, token, userName });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
+    if (!errors.isEmpty()) {
+      const cleanErrors = errors.errors.map((error) => error.msg);
+      console.log(cleanErrors);
+      res.status(400).json({ error: cleanErrors });
+    } else {
+      try {
+        const user = await User.login(email, password);
+        const userName = user.userName;
+
+        // create token
+        const token = createToken(user._id, user.userName);
+
+        res.status(200).json({ email, token, userName });
+      } catch (err) {
+        res.status(400).json({ error: [err.message] });
+      }
+    }
+  },
+];
