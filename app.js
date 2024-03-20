9ce68cab1ca5d96d4dccf449e9592d7676a85eb3
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-20 11:39:01 -0400

    Removed console.log

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
-      console.log(origin);
     } else {
-      console.log(origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
